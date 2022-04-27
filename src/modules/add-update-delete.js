2b794879f115262b642b1262efbc60211f56e926
Kelvin Bush <kelybush@gmail.com>
2022-04-27 12:18:05 +0000

    set up delete task from list functionality
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

--- src/modules/add-update-delete.js
+++ src/modules/add-update-delete.js
@@ -1,5 +1,5 @@
 export const addToList = (list) => {
-  const input = document.querySelector('#input');
+  const input = document.querySelector('#todo');
   const { value } = input;
   if (value) {
     list.addToDo(value);
@@ -13,4 +13,12 @@ export const removeCompletedFromList = (list) => {
 
 export const updateCompleted = (list, id) => {
   list.updateToDoCompleted(id);
+};
+
+export const updateDescription = (list, id, description) => {
+  list.updateDescription(+id, description);
+};
+
+export const removeFromList = (list, id) => {
+  list.removeToDo(+id);
 };
