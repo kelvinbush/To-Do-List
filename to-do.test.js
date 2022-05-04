f2cc207c466b0d6c9311308f562e04b8216aecab
lUCI <lusindisontanjana@gmail.com>
2022-05-04 14:30:00 +0200

    Added test for the add and remove functionality

--- to-do.test.js
+++ to-do.test.js
@@ -11,3 +11,21 @@ describe('ToDoList', () => {
     localStorage.clear();
   });
 });
+
+it('should add a new task to the list', () => {
+  toDoList.addToDo('test');
+  document.querySelector('#to-do-list').innerHTML = toDoList.renderToDos();
+  const list = document.querySelectorAll('#to-do-list .container');
+  expect(list)
+    .toHaveLength(1);
+});
+
+it('should remove a task from the list', () => {
+  toDoList.addToDo('test');
+  toDoList.addToDo('test2');
+  toDoList.removeToDo(1);
+  document.querySelector('#to-do-list').innerHTML = toDoList.renderToDos();
+  const list = document.querySelectorAll('#to-do-list .container');
+  expect(list)
+    .toHaveLength(1);
+});
