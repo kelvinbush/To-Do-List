3b0bc7c27a6bc834d8dbc36677fff9c10192e2b0
lUCI <lusindisontanjana@gmail.com>
2022-05-05 09:48:19 +0200

    Implement updateCompleted test

---
+++ to-do.test.js
@@ -0,0 +1,41 @@
+/**
+ * @jest-environment jsdom
+ */
+import ToDoList from './src/modules/to-do-list.js';
+
+describe('ToDoList', () => {
+  let toDoList;
+  beforeEach(() => {
+    toDoList = new ToDoList();
+    document.body.innerHTML = '<ul id="to-do-list"></ul>';
+    localStorage.clear();
+  });
+
+  it('should add a new task to the list', () => {
+    toDoList.addToDo('test');
+    document.querySelector('#to-do-list').innerHTML = toDoList.renderToDos();
+    const list = document.querySelectorAll('#to-do-list .container');
+    expect(list)
+      .toHaveLength(1);
+  });
+
+  it('should remove a task from the list', () => {
+    toDoList.addToDo('test');
+    toDoList.addToDo('test2');
+    toDoList.removeToDo(1);
+    document.querySelector('#to-do-list').innerHTML = toDoList.renderToDos();
+    const list = document.querySelectorAll('#to-do-list .container');
+    expect(list)
+      .toHaveLength(1);
+  });
+
+  est('updateCompleted', () => {
+    toDoList.addToDo('task1');
+    toDoList.addToDo('task2');
+    toDoList.addToDo('task3');
+    toDoList.updateToDoCompleted(1);
+    document.querySelector('#to-do-list').innerHTML = toDoList.renderToDos();
+    expect(document.querySelectorAll('#to-do-list .container')[0].children[0].classList.contains('fa-check'))
+      .toBe(true);
+  });
+});
