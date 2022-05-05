6991f956843f00c781424df29e6d29076b872ca3
Kelvin Bush <kelybush@gmail.com>
2022-05-05 07:45:24 +0000

    implement edit task test
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

--- edit-update.test.js
+++ edit-update.test.js
@@ -10,4 +10,15 @@ describe('ToDoList', () => {
     document.body.innerHTML = '<ul id="to-do-list"></ul>';
     localStorage.clear();
   });
+
+  test('editTask', () => {
+    toDoList.addToDo('task1');
+    toDoList.addToDo('task2');
+    toDoList.addToDo('task3');
+    toDoList.updateDescription(1, 'task1 edited');
+    document.querySelector('#to-do-list').innerHTML = toDoList.renderToDos();
+    expect(document.querySelectorAll('#to-do-list .container')[0].children[1].value)
+      .toBe('task1 edited');
+  });
+
 });
